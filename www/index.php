490e26fdf6f563a2c12ac450b887b78fa6723ef0
Sang CNT <sangcnt@gmail.com>
2022-08-19 21:22:05 +0700

    Update www

--- www/index.php
+++ www/index.php
@@ -9,15 +9,23 @@
     </head>
 
     <body>
-        <div class="container">
-            <form class="pt-3">
-                <div class="mb-3">
-                    <textarea id="log-board" rows="10" class="form-control" id="validationTextarea" readonly></textarea>
+        <div class="container vh-100 pt-2 d-flex flex-column">
+            <ul class="nav nav-pills nav-justified" id="pills-tab" role="tablist">
+                <li class="nav-item" role="presentation">
+                    <button class="nav-link active" id="pills-trades-tab" data-bs-toggle="pill" data-bs-target="#pills-trades" type="button" role="tab" aria-controls="pills-trades" aria-selected="true">Home</button>
+                </li>
+                <li class="nav-item" role="presentation">
+                    <button class="nav-link" id="pills-board-tab" data-bs-toggle="pill" data-bs-target="#pills-board" type="button" role="tab" aria-controls="pills-board" aria-selected="false">Profile</button>
+                </li>
+            </ul>
+            <div id="pills-tabContent" class="tab-content flex-fill py-2">
+                <div class="tab-pane fade h-100 show active" id="pills-trades" role="tabpanel" aria-labelledby="pills-trades-tab">
+                    <div id="log-trades" class="small h-100 border border-secondary p-2 rounded" id="validationTextarea" readonly></div>
                 </div>
-                <div class="mb-3">
-                    <textarea id="log-trades" rows="10" class="form-control" id="validationTextarea" readonly></textarea>
+                <div class="tab-pane fade h-100" id="pills-board" role="tabpanel" aria-labelledby="pills-board-tab">
+                    <div id="log-board" class="small h-100 border border-secondary p-2 rounded" id="validationTextarea" readonly></div>
                 </div>
-            </form>
+            </div>
         </div>
 
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
@@ -40,7 +48,7 @@
                         if (data.board_lines.length > 0) {
                             boardLine = data.board_line_count + 1;
                             $.each(data.board_lines, function(index, line) {
-                                logBoard.val(line + logBoard.val());
+                                logBoard.html(line + "<br>" + logBoard.html());
                             });
                         }
 
